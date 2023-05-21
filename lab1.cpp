bb5d1583ff653887e183121ba14a9bcc680ce131
Grandkojo <103688380+Grandkojo@users.noreply.github.com>
2023-05-21 13:34:40 +0000

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int number = 17785;
+    char name[50] = "Welcome to this class!";
+    float neg_float = -56.1427;
+
+    int x = 5,y = 5,z = 5;
+    bool statement = true;
+    string  mix = "5262778y";
+    char str_array[9];
+
+    int age, current_year = 2023, birth_year;
+    cout<< "Enter your age: ";
+    cin >> age;
+    birth_year = current_year - age;
+    cout<<"\nYou were born in the year "<< birth_year<<endl;
+
+    int marks[] = {67, 71, 54, 43, 82, 90};
+    marks[2] = 75;
+    int AppliedElectricity = marks[4];
+    cout << "The third value is "<< marks[2]<< " and the fifth value is "<< AppliedElectricity;
+    marks[9]= 100;
+    cout <<endl<< marks[9];
+    string my_string[10] = {"Hello", "World"};
+    int x, y, z = 5;
+    cout<<"x" << x<< "y" << y<< "z"<<z;
+    return 0;
+}
